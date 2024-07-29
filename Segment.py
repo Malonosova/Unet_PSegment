2e8dadbc93d7c7c0c2ac5e7fa55c5c0ff6b93bee
Malonosova <32372730+Malonosova@users.noreply.github.com>
2024-07-29 04:02:54 +0300

    Add files via upload

--- Segment.py
+++ Segment.py
@@ -6,7 +6,7 @@ import torch
 import numpy as np
 
 class Segmentation:
-    def __init__(self, model, loss, optimizer, epochs=200, device='cuda', save_path="./save_model/"):
+    def __init__(self, model, loss, optimizer, epochs=100, device='cuda', save_path="./save_model/"):
         self.device = torch.device(device)
         self.model = model.to(self.device)
         self.loss_fn = loss.to(self.device)
@@ -36,7 +36,7 @@ class Segmentation:
         return iou(pred.argmax(dim=1), target)
     
     def save_txt(self, history):
-        with open('out_lr4_.txt', 'w') as f:
+        with open('out_lr1_.txt', 'w') as f:
             f.write('epoch\ttrain_loss\ttrain_iou\tval_iou\n')
             for ind, i in enumerate(np.array(history).T):
                 f.write(str(ind)+'\t')
@@ -45,10 +45,11 @@ class Segmentation:
                 f.write('\n')
             f.close
     
-    def Plot_train(self, loss, iou_train, iou_val):
+    def Plot_train(self, loss, val_loss, iou_train, iou_val):
         plt.figure(figsize=(18, 5))
         plt.subplot(1, 3, 1)
-        plt.plot(loss, )
+        plt.plot(np.array([loss, val_loss]).T)
+        plt.legend(['train loss', 'val loss'])
         plt.title('loss')
 
         plt.subplot(1, 3, 2)
@@ -64,7 +65,7 @@ class Segmentation:
         
     def evaluation(self, model, val_loader):
         model.eval()
-        avg_val_iou = []
+        avg_val_loss, avg_val_iou = 0, []
         with torch.no_grad():
             for x_batch, y_batch in val_loader:
                 x_batch = x_batch.to(self.device)
@@ -72,16 +73,20 @@ class Segmentation:
                 y_batch_body, y_batch_half = self.split(y_batch)
 
                 out0, out1, out2 = model(x_batch)
-
+                loss = self.loss_fn(out0,y_batch_body)+2*self.loss_fn(out1,y_batch_half)+5*self.loss_fn(out2,y_batch)
+                
                 avg_iou0 = self.metric(out0, y_batch_body, 2)
                 avg_iou1 = self.metric(out1, y_batch_half, 3)
                 avg_iou2 = self.metric(out2, y_batch, 7)
                 
                 avg_val_iou.append([avg_iou0.cpu(), avg_iou1.cpu(), avg_iou2.cpu()])
-        return avg_val_iou
+                avg_val_loss += loss
+                
+            avg_val_loss /= len(val_loader)
+        return avg_val_iou, avg_val_loss
         
     def fit(self, train_loader, val_loader):
-        train_loss, train_iou, val_iou = [], [], []
+        train_loss, val_loss, train_iou, val_iou = [], [], [], []
         
         for i in tqdm(range(self.epochs)):
             avg_train_loss, avg_train_iou = 0, []
@@ -108,19 +113,16 @@ class Segmentation:
                 avg_train_iou.append([avg_iou0.cpu(), avg_iou1.cpu(), avg_iou2.cpu()])
                 
             avg_train_loss /= len(train_loader)
-            avg_val_iou = self.evaluation(self.model, val_loader)
+            avg_val_iou, avg_val_loss = self.evaluation(self.model, val_loader)
             
             train_loss.append(round(avg_train_loss.item(), 4))
+            val_loss.append(round(avg_val_loss.item(), 4))
             train_iou.append(np.mean(avg_train_iou, axis=0))
             val_iou.append(np.mean(avg_val_iou, axis=0))
             
             if i%10==0:
-#                 print('train loss: ', round(avg_train_loss.item(), 4), 
-#                       'train acc: ', np.mean(avg_train_acc, axis=0), 
-#                       'val acc: ', np.mean(avg_val_acc, axis=0))
                 clear_output(wait=True)
-                self.Plot_train(train_loss, train_iou, val_iou)
-                if i>100:
-                    torch.save(model.state_dict(), self.save_path+f'model_lr4_epochs_{i}')
+                self.Plot_train(train_loss, val_loss, train_iou, val_iou)
+                torch.save(self.model.state_dict(), self.save_path+f'model_lr1_epochs_{i}')
         self.save_txt([train_loss, train_iou, val_iou])
         return train_loss, train_iou, val_iou
